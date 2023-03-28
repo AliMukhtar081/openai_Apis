0f2160831efcb3a598832c1037c13eb7b3062f45
AliMukhtar081 <AliMukhtar081@gmail.com>
2023-03-28 13:53:03 +0500

    Tweet GPT-3

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const openai = require('openai');
+
+// Set your OpenAI API key
+openai.apiKey = process.env.OPENAI_API_KEY;
+
+// Define the prompt
+const prompt = "Generate a tweet based on this context:";
+
+// Read the CSV file containing tweets and split into an array of contexts
+const tweets = fs.readFileSync('tweets.csv', 'utf-8').split('\n');
+
+// Generate text using GPT-3 for each context
+for (const context of tweets) {
+  openai.Completion.create({
+    engine: 'davinci',
+    prompt: `${prompt}\n${context}`,
+    maxTokens: 100,
+    n: 1,
+    temperature: 0.5,
+  })
+  .then((response) => {
+    // Print the generated text
+    console.log(response.choices[0].text.trim());
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
